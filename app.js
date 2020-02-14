342053635494c6e57c99a85e594f5d612a70d8b6
Foster <Erou.Foster@financialpartners.com>
2020-02-14 17:32:29 -0500

    bug fixes, multifile loader

--- app.js
+++ app.js
@@ -7,24 +7,62 @@ const iseEndPointsEndpointUpdate = require('./lib/iseInteractions/iseEndPointsEn
 const metaDataUpdate = require('./lib/metaDataUpdate.js');
 const LogGenerator = require('./lib/LogGenerator.js');
 const data = require('./import/fciFacilities.json');
-const {objectKeyFilter} = require("nodeutilz");
+const {objectKeyFilter,dirContents,readFile} = require("nodeutilz");
+const cliProgress = require('cli-progress');
+const _colors = require('colors');
 
-iseEndPointsMacLookupDetail
+
 const dotenv = require('dotenv').config();
 const iseAuth = process.env.ISE_AUTH;
 const iseServer = process.env.ISE_SERVER;
 const debugAndTest = false;
 
 
-const what = new LogGenerator('deviceGroupUpdate',"applog",{debug:debugAndTest})
-const {payload,groupId,description} = data;
+const multibar = new cliProgress.MultiBar({
+    format: '{barName} |' + _colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Chunks || Speed: {speed}',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+});
+const what = new LogGenerator('deviceGroupUpdate',"applog",{debug:debugAndTest});
 
-iseConnectionTest(iseServer, iseAuth, {payload} ,false)
-.then((t) => metaDataUpdate(t,{groupId,description,test:debugAndTest}))
-.then(iseEndPointsMacLookup)
-.then(iseEndPointsMacLookupDetail)
-.then(iseEndPointsEndpointUpdate)
-.then(({payload,metaData}) => ({applog:{"payload":payload, metaData}}))
-.then(what.log.bind(what))
-.then(console.log)
-.catch(console.error)
+
+return dirContents('./import/bulkJson')
+.then((t) => Promise.all(t.map((d) => readFile(`./import/bulkJson/${d}`))))
+.then((t) => Promise.all(t.map((d) => JSON.parse(d))))
+.then((t) => {
+    return Promise.all(t.map(({payload,groupId,description}) => {
+        
+        return iseConnectionTest(iseServer, iseAuth, {payload} ,false)
+        .then((t) => metaDataUpdate(t,{groupId,description,test:debugAndTest,rejects:[],multibar,barId:description}))
+        .then(iseEndPointsMacLookup)
+        .then(iseEndPointsMacLookupDetail)
+        .then(iseEndPointsEndpointUpdate)
+        .then(({payload,metaData}) => ({applog:{"payload":payload, metaData}}))
+        .then(what.log.bind(what))
+        //.then(console.log)
+        .catch(console.error)
+    }))
+})
+//.then(console.log)
+.catch(console.log)
+
+// iseEndPointsMacLookupDetail
+// const dotenv = require('dotenv').config();
+// const iseAuth = process.env.ISE_AUTH;
+// const iseServer = process.env.ISE_SERVER;
+// const debugAndTest = false;
+
+
+// const what = new LogGenerator('deviceGroupUpdate',"applog",{debug:debugAndTest})
+// const {payload,groupId,description} = data;
+
+// iseConnectionTest(iseServer, iseAuth, {payload} ,false)
+// .then((t) => metaDataUpdate(t,{groupId,description,test:debugAndTest}))
+// .then(iseEndPointsMacLookup)
+// .then(iseEndPointsMacLookupDetail)
+// .then(iseEndPointsEndpointUpdate)
+// .then(({payload,metaData}) => ({applog:{"payload":payload, metaData}}))
+// .then(what.log.bind(what))
+// .then(console.log)
+// .catch(console.error)
